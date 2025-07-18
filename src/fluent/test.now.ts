dc43490f3beed9accd0dfee90292e977abd35576
System Administrator <admin@example.com>
2025-07-18 14:27:38 +0530

    commit

--- src/fluent/test.now.ts
+++ src/fluent/test.now.ts
@@ -33,7 +33,7 @@ BusinessRule({
     name: 'testbr',
     table: 'x_1436243_ideunive_test',
     when: 'after',
-    action: ["update","delete","insert"],
+    action: ["delete","insert"],
     script: showStateUpdate,
     order: 100,
     active: true,
