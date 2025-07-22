e368eb8ec220cc13ffe0544b51d46066aeb8c265
System Administrator <admin@example.com>
2025-07-22 15:20:48 +0530

    Commit

---
+++ src/fluent/generated/sys_package_dependency_m2m_c62d521e83ee6a107908f865eeaad373.now.ts
@@ -0,0 +1,10 @@
+import { Record } from '@servicenow/sdk/core'
+
+export default Record({
+    $id: 'c62d521e83ee6a107908f865eeaad373',
+    table: 'sys_package_dependency_m2m',
+    data: {
+        dependency: 'ea9496a4c8d1221055906c7518aa2d07',
+        min_version: '1.0.0',
+    },
+})
